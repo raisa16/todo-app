0e51e14a9023ecba1124b58b75b3f2c2855d59cc
Raisa Toscano <daisa1690@gmail.com>
2025-08-25 00:35:14 -0600

    add home and labs components and  use string interpolation nd data binding for labs

---
+++ src/app/pages/labs/labs.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+@Component({
+  selector: 'app-labs',
+  imports: [CommonModule],
+  templateUrl: './labs.html',
+  styleUrl: './labs.css'
+})
+export class Labs {
+title = "List of tasks";
+tasks = [
+  'Install Angular CLI',
+  'Crear proyecto',
+  'Crear componentes'
+]
+name = 'Mia'
+age = 14;
+disabled = true;
+img = "https://w3schools.com/howto/img_avatar.png";
+
+person = {
+  name: 'Mia',
+  age: 14,
+  avatar: 'https://w3schools.com/howto/img_avatar.png'
+}
+
+}
