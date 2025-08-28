cc1bb34b381ac36c08ea890e4158ca4eabbaa9be
Raisa Toscano <daisa1690@gmail.com>
2025-08-28 02:30:10 -0600

    add addTask, deleteTask and updateTask methods and  task.model  interface and add json pipe to debug code

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { Task } from '../../models/task.model';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [  JsonPipe],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class Home {
-
+tasks = signal<Task[]>( [
+  {
+    id: Date.now(),
+    title: 'Install Angular CLI',
+    completed: false
+  },
+  {
+    id: Date.now(),
+    title: 'Crear proyecto',
+    completed: false
+  },
+  {
+    id: Date.now(),
+    title: 'Crear componentes',
+    completed: false
+  }  
+]);
+changeHandler(event:Event) {
+  const newValue = event.target as HTMLInputElement;
+  const task = newValue.value;
+  this.addTask(task);
+  
 }
+addTask(task: string){
+  const newTask = {
+    id: Date.now(),
+    title: task,
+    completed: false
+  }
+  this.tasks.update((tasks) => [...tasks, newTask])
+}
+deleteTask(index: number) {
+  this.tasks.update(tasks => tasks.filter((task,position) => position !== index))
+}
+updateTask(index: number) {
+  this.tasks.update(tasks => {
+    return tasks.map((task,position) => {
+    if(position === index){
+      return {
+        ...task,
+        completed: !task.completed
+      }
+    }
+    return task;
+  })
+})
+}
+}
+
+
